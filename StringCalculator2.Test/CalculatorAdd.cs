b0dbf518f1b0fceff9e644bd9574dbfd7dd2a96c
Shmuel <samuelwine@gmail.com>
2019-12-25 13:09:30 +0000

    commit just before changing string.split to regex extraction

--- StringCalculator2.Test/CalculatorAdd.cs
+++ StringCalculator2.Test/CalculatorAdd.cs
@@ -11,7 +11,9 @@ namespace StringCalculator2.Test
         [InlineData("1,2", 3)]
         [InlineData("1,2,3,7,9,15", 37)]
         [InlineData("1\n2,3", 6)]
-        [InlineData("//;\n1;2", 3)]        
+        [InlineData("//;\n1;2", 3)]       
+        [InlineData("1001,2", 2)]
+        [InlineData("//[|||]1|||2|||3", 6)]
         public void ReturnsCorrectOutput(string input, int output)
         {
             // Arrange
@@ -26,17 +28,15 @@ namespace StringCalculator2.Test
 
         [Theory]
         [InlineData("-1,2", "Negatives not allowed: -1")]
+        [InlineData("2,-4,3,-5", "Negatives not allowed: -4,-5")]
         public void ReturnsErrorMessageForNegativeInputsWithDetails(string input, string output)
         {
             //  Arrange
             Calculator sut = new Calculator();
-
-            //  Act
-            var result = sut.Add(input);
-
+            
             //  Assert
             var exception = Assert.Throws<ArgumentOutOfRangeException>(() => sut.Add(input));
-            Assert.Equal(output, exception.Message);
+            Assert.Contains(output, exception.Message);
         }
         
         
--- StringCalculator2/Calculator.cs
+++ StringCalculator2/Calculator.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace StringCalculator2
 {
@@ -7,26 +9,74 @@ namespace StringCalculator2
         public int Add(string numbers)
         {
             var total = 0;
-            char[] delimiterArray;
-            if (numbers.StartsWith("//"))
-            { 
-                delimiterArray = new char[] {',', '\n', numbers[2]};
+            int result;
+            
+            List<int> negativeValues = new List<int>();
+
+            var delimiterArray = CreateDelimiterArray(numbers);
+            var stringArray = numbers.Split(delimiterArray, StringSplitOptions.None);
+            foreach (var str in stringArray)
+            {                
+                int.TryParse(str, out result);                
+                if (result < 0) negativeValues.Add(result);
+                if (result > 1000) continue;
+                else total += result;              
+            }
+
+            if (negativeValues.Count > 0)
+            {
+                var message = "Negatives not allowed: ";
+                foreach (var value in negativeValues)
+                {
+                    message += $"{value},";
+                }
+                message.TrimEnd(',');
+                throw new ArgumentOutOfRangeException(nameof(negativeValues), message);
+            }
+            else return total;
+
+
+        }
+
+        private string[] CreateDelimiterArray(string numbers)
+        {
+            string[] delimiterArray;
+            string searchPattern = "^//\\D\n";
+            string extendedSearchPattern = "^//[\\D+]";
+
+            if (Regex.IsMatch(numbers, searchPattern))
+            {
+                delimiterArray = new string[] {",", "\n", numbers[2].ToString()};
+            }
+
+            else if (Regex.IsMatch(numbers, extendedSearchPattern))
+            {
+                var delimiterStartIndex = numbers.IndexOf('[') + 1;
+                var delimiterEndIndex = numbers.IndexOf(']') - 1;
+                var delimiterLength = delimiterEndIndex - delimiterStartIndex + 1;
+                var newDelimiter = numbers.Substring(delimiterStartIndex, delimiterLength);                
+                delimiterArray = new string[] { ",", "\n", newDelimiter };
             }
 
             else
             {
-                delimiterArray = new char[] {',', '\n'};
+                delimiterArray = new string[] { ",", "\n" };
             }
 
-            var stringArray = numbers.Split(delimiterArray);
-            foreach (var str in stringArray)
-            {
-                int result;
-                int.TryParse(str, out result);
-                total += result;
-            }
-
-            return total;            
+            return delimiterArray;
         }
+
+        //private string[] ExtractNumericSubStrings(string numbers)
+        //{
+        //    string[] resultArray;
+        //    string[] patterns = { "^//\\D\n", "^//[\\D+]" };
+        //    foreach (var pattern in patterns)
+        //    {
+        //        if (Regex.IsMatch(numbers, pattern))
+        //        {
+        //            resultArray = 
+        //        }
+        //    }
+        //}
     }
 }
